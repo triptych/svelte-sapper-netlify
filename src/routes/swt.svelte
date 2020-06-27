fd0fcb786c963a66ef0de82c461eed3c630c81e5
Andrew Wooldridge <triptych@gmail.com>
2020-06-26 22:56:20 -0700

    adding The Things page

---
+++ src/routes/swt.svelte
@@ -0,0 +1,17 @@
+<style>
+    .thing_grid{
+        display: flex;
+    }
+</style>
+
+<svelte:head>
+	<title>Small Web Things</title>
+</svelte:head>
+
+<h1>The Things</h1>
+
+<div class="thing_grid">
+    <div class="element"><a href="https://indiedevstudio.com/">Indie Dev Studio</a></div>
+
+
+</div>
